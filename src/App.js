cf40482f929530b537c68a7b74bad1b36623f385
이름 <galeb12243@gmail.com>
2023-07-13 12:32:31 +0900

    change text

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="" // 주소 입력
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          시작하기
         </a>
       </header>
     </div>
